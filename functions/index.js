fb7905033630e74f6eb429110cf79cf4bdfd3e20
Richard Whitney <rwhitney32@gmail.com>
2020-02-05 00:11:10 +0000

    Added signup and login endpoints

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,32 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
-var serviceAccount = require("./lab-fyp-rw-f55f36454a34");
+require('dotenv');
+const serviceAccount = require("./lab-fyp-rw-f55f36454a34");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://lab-fyp-rw.firebaseio.com"
 });
 
-const express = require('express');
-const app = express();
+const app = require('express')();
+const db = admin.firestore();
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAdeDJb7BesLOcfHvEB6z-zNHatrhfzojk",
+  authDomain: "lab-fyp-rw.firebaseapp.com",
+  databaseURL: "https://lab-fyp-rw.firebaseio.com",
+  projectId: "lab-fyp-rw",
+  storageBucket: "lab-fyp-rw.appspot.com",
+  messagingSenderId: "81018625605",
+  appId: "1:81018625605:web:db87a216a35f7ad7838d2c",
+  measurementId: "G-D7GC08D760"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
 
 app.get('/tests', (request, response) => {
-  admin.firestore()
+  db
     .collection('tests')
     .orderBy('name')
     .get()
@@ -46,8 +60,7 @@ app.post('/test', (request, response) => {
     turnaroundTime: request.body.turnaroundTime
   };
 
-  admin
-    .firestore()
+  db
     .collection('tests')
     .add(newTest)
     .then(doc => {
@@ -58,5 +71,91 @@ app.post('/test', (request, response) => {
       console.error(err);
     });
 });
+
+// Signup route
+app.post('/signup', (request, response) => {
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmpPassword: request.body.confirmpPassword,
+    handle: request.body.handle
+  };
+  // TODO validate data
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty'
+  } else  if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address'
+  }
+  if (isEmpty(newUser.password)) {
+    errors.password = 'Must not be empty'
+  }
+  if (newUser.password !== newUser.confirmpPassword) {
+    errors.confirmPassword = 'Passwords must match'
+  }
+  if (isEmpty(newUser.handle)) {
+    errors.handle = 'Must not be empty'
+  }
+  if (Object.keys(errors).length > 0) return response.status(400).json(errors);
+
+  let newToken;
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      newToken = token;
+      return response.status(201).json({token});
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === 'auth/email-already-in-use') {
+        return response.status(400).json({email: 'Email is already in use'});
+      }
+      return response.status(500).json({error: err.code});
+    });
+});
+
+// Login route
+app.post('/login', (request, response) => {
+  const user = {
+    email: request.body.email,
+    password: request.body.password
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) errors.email = 'Must not be empty';
+  if (isEmpty(user.password)) errors.password = 'Must not be empty';
+  if (Object.keys(errors).length > 0) return response.status(400).json(errors);
+
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return response.json({token});
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === 'auth/user-not-found') {
+        return response.status(403).json({general: 'Email not recognised, please try again'});
+      }
+      if (err.code === 'auth/wrong-password') {
+        return response.status(403).json({general: 'Credentials not recognised, please try again'});
+      }
+      return response.status(500).json({error: err.code});
+    })
+});
+
+const isEmpty = (string) => {
+  return string.trim() === '';
+};
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return !!email.match(regEx);
+};
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
